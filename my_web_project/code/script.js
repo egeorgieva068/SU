0bf5726aa6b1f4aea2f31b10791056131dbda221
egeorgieva068 <egeorgieva068@gmail.com>
2026-03-16 12:28:08 +0200

    Init repo with html&css view

---
+++ my_web_project/code/script.js
@@ -0,0 +1,9 @@
+
+function showWelcome() {
+  alert("Здравей! Това е по-модерен студентски проект по HTML, CSS и JavaScript.");
+}
+
+function submitMessage(event) {
+  event.preventDefault();
+  alert("Съобщението е изпратено успешно.");
+}
